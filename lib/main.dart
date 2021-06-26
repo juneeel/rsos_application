f3c74c01f6a9f2e4a1f1a3adfe232b60b50c54f2
juneeel <61371369+juneeel@users.noreply.github.com>
2021-06-26 16:43:55 +0800

    lates

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       theme: ThemeData(
           primaryColor: Colors.orange
       ),
